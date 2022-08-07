95396ab7518fae0cbf510fae5879c4db395e6b0b
Geogonal <86677753+Geogonal@users.noreply.github.com>
2022-08-06 21:52:17 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -17,6 +17,24 @@ function handler(plr)
         end
     end
 end
+function update()
+    teamcolor = game.Players.LocalPlayer.TeamColor
+    for i, v in pairs(game.Workspace.Players:GetChildren()) do
+        if v.Name ~= teamcolor then
+            for i2, v2 in pairs(v:GetChildren()) do
+                if enabled then
+                    v2[part].Size = Vector3.new(size, size, size)
+                    v2[part].Mesh.Scale = Vector3.new(size, size, size)
+                    v2[part].Transparency = trans
+                else
+                    v2[part].Size = Vector3.new(size, size, size)
+                    v2[part].Mesh.Scale = Vector3.new(1, 1, 1)
+                    v2[part].Transparency = 0
+                end
+            end
+        end
+    end
+end
 Tab:AddToggle({
 	Name = "Hitbox extender",
 	Default = false,
@@ -29,34 +47,14 @@ Tab:AddToggle({
         end
   	end
 })
-
-function update()
-    teamcolor = game.Players.LocalPlayer.TeamColor
-    for i, v in pairs(game.Workspace.Players:GetChildren()) do
-        if v.Name ~= teamcolor then
-            for i2, v2 in pairs(v:GetChildren()) do
-                if enabled then
-                    v2[part].Size = Vector3.new(size, size, size)
-                    v2[part].Mesh.Scale = Vector3.new(size, size, size)
-                    v2.Transparency = trans
-                else
-                    v2[part].Size = Vector3.new(size, size, size)
-                    v2[part].Mesh.Scale = Vector3.new(1, 1, 1)
-                    v2.Transparency = 0
-                end
-            end
-        end
-    end
-end
-
 Tab:AddSlider({
     Name = "Size(studs)",
     Min = 5,
     Max = 35,
     Default = 10,
-    Color = Color3.fromRGB(255,255,255),
+    Color = Color3.fromRGB(255, 255, 255),
     Increment = 1,
-    ValueName = 'bananas',
+    ValueName = 'studs',
     Callback = function(val)
         update()
         size = val
@@ -69,24 +67,20 @@ Tab:AddSlider({
     Default = 0.5,
     Color = Color3.fromRGB(255,255,255),
     Increment = 0.01,
-    ValueName = 'bananas',
+    ValueName = '',
     Callback = function(val)
         update()
         trans = val
     end
 })
-
-
-wait(0.05)
-
 for i, v in pairs(game.Workspace.Players:GetChildren()) do
     if enabled then
         teamcolor = game.Players.LocalPlayer.TeamColor
         if v.Name ~= teamcolor then
             for i2, v2 in pairs(v:GetChildren()) do
-                v2.Torso.Size = Vector3.new(size, size, size)
+                v2[part].Size = Vector3.new(size, size, size)
                 v2[part].Mesh.Scale = Vector3.new(size, size, size)
-                v2.Torso.Transparency = trans
+                v2[part].Transparency = trans
             end
         end
     end
@@ -101,3 +95,4 @@ game.Workspace.Players["Bright orange"].ChildAdded:Connect(function(child)
         handler(child)
     end 
 end)
+OrionLib:Init()
